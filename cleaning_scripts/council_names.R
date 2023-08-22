f6349b76e5d21ab09ca0ec7a92201e4e1866dcdb
Emmz900 <68696398+Emmz900@users.noreply.github.com>
2023-08-22 10:26:48 +0100

    joined neighbourhood and community datasets

--- cleaning_scripts/council_names.R
+++ cleaning_scripts/council_names.R
@@ -5,6 +5,7 @@ council_areas <- clean_names(read_csv("raw_data/council_area_codes.csv"))
 
 council_areas_clean <- council_areas %>% 
   select(ca, ca_name) %>% 
-  add_row(ca = "S92000003" ,ca_name = "Scotland")
+  add_row(ca = "S92000003" ,ca_name = "Scotland") %>% 
+  distinct() # otherwise there are duplicates
 
 write_csv(council_areas_clean, "clean_data/council_areas.csv")
